5e55df8f0ae4ca302b2c11fc910c49494ff59d8d
Julius-Okeyo <jaykopiyo@gmail.com>
2022-07-03 14:21:03 +0300

    Updated GIF.py, reduced duration from 10s to 1s

--- GIF.py
+++ GIF.py
@@ -4,4 +4,4 @@ filenames = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg']
 images = []
 for filename in filenames:
     images.append(imageio.imread(filename))
-imageio.mimsave('Datascience.gif',images,'GIF',duration=10)
+imageio.mimsave('Datascience.gif',images,'GIF',duration=1)
